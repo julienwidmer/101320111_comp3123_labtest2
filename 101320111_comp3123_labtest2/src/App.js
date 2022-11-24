3ecf76c4613bd3d58d0b9ab0093d0578245a8ca2
Julien Widmer <ju.widmer@pm.me>
2022-11-24 16:45:06 -0500

    Add current weather info

--- 101320111_comp3123_labtest2/src/App.js
+++ 101320111_comp3123_labtest2/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import TorontoCurrentWeather from "./components/TorontoCurrentWeather";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container" >
+        <div className="row justify-content-center align-items-center" style={{"height": "100vh"}}>
+                <div className="col-auto justify-content-center rounded border d-flex flex-column">
+                    <div className="px-4 pt-3 pb-2">
+                        <h1>Toronto</h1>
+                        <p>Weather info provided by the Open Weather API</p>
+                    </div>
+                    <div className="row px-4 pb-4 rounded d-flex">
+                        <TorontoCurrentWeather/>
+                    </div>
+                </div>
+        </div>
     </div>
   );
 }
